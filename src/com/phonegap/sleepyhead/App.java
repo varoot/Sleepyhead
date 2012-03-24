d2882553266a8de957a37d8b5dbc6bfd55a71379
Pei-Yao Hung <peiyaoh@gmail.com>
2012-03-24 13:29:37 -0400

    Try to do the experience sampling page, not finish yet. Need to work on the time format

--- src/com/phonegap/sleepyhead/App.java
+++ src/com/phonegap/sleepyhead/App.java
@@ -8,6 +8,7 @@ public class App extends DroidGap {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        super.loadUrl("file:///android_asset/www/index.html");
+        //super.loadUrl("file:///android_asset/www/index.html");
+        super.loadUrl("file:///android_asset/www/exp.html");
     }
 }
