dbd885c7647de66ad436fd6e714c14f05cf90da3
Pei-Yao Hung <peiyaoh@gmail.com>
2012-03-27 15:11:11 -0400

    Change self-report and exp format

--- src/com/phonegap/sleepyhead/App.java
+++ src/com/phonegap/sleepyhead/App.java
@@ -8,7 +8,7 @@ public class App extends DroidGap {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //super.loadUrl("file:///android_asset/www/index.html");
+        //super.loadUrl("file:///android_asset/www/self_report.html");
         super.loadUrl("file:///android_asset/www/exp.html");
     }
 }
