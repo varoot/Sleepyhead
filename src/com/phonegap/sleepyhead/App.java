0b9495fb54fbbfc3cd14bfc1e85a5bc137f58976
Pei-Yao Hung <peiyaoh@gmail.com>
2012-03-30 01:14:30 -0400

    Try to add system notification code, but dose not work properly

--- src/com/phonegap/sleepyhead/App.java
+++ src/com/phonegap/sleepyhead/App.java
@@ -1,14 +1,16 @@
 package com.phonegap.sleepyhead;
 
 import android.os.Bundle;
-import com.phonegap.*;
+
+import com.phonegap.DroidGap;
 
 public class App extends DroidGap {
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //super.loadUrl("file:///android_asset/www/self_report.html");
-        super.loadUrl("file:///android_asset/www/exp.html");
+        super.loadUrl("file:///android_asset/www/self_report.html");
+        //super.loadUrl("file:///android_asset/www/exp.html");
+        super.addService("systemNotification","com.phonegap.helloworld.SystemNotification");
     }
 }
