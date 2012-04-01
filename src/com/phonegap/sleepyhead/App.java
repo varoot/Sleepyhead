7e00230de1b6b05d602ef869bb3c60b4f42d3977
Pei-Yao Hung <peiyaoh@gmail.com>
2012-04-01 11:43:23 -0400

    index.html

--- src/com/phonegap/sleepyhead/App.java
+++ src/com/phonegap/sleepyhead/App.java
@@ -9,9 +9,9 @@ public class App extends DroidGap {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //super.loadUrl("file:///android_asset/www/index.html");
+        super.loadUrl("file:///android_asset/www/index.html");
         //super.loadUrl("file:///android_asset/www/exp.html");
-        super.loadUrl("file:///android_asset/www/activity_track.html");
+        //super.loadUrl("file:///android_asset/www/activity_track.html");
         super.addService("systemNotification","com.phonegap.sleepyhead.SystemNotification");
     }
 }
